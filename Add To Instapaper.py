40ca6a27af6edb06cfe4ec69020312b10675e54a
Peteous <erik.peterman@und.edu>
2017-03-03 11:36:45 -0600

    Added logic to run outside of Pythonista

--- Add To Instapaper.py
+++ Add To Instapaper.py
@@ -1,8 +1,11 @@
 
 #These libraries are available in Pythonista for iOS
-import appex
-import requests
-import clipboard
+try:
+	import appex
+	import requests
+	import clipboard
+except ImportError:
+	print('You appear to not be using Pythonista')
 
 #import urlEncode method from my Encode file and make urlEncode method if import fails
 try:
@@ -32,9 +35,18 @@ def getInstapaperURL(webAddress):
 	webAddress = str(webAddress)
 	instapaper = 'https://www.instapaper.com/api/add'
 	
-	#The username and password should be altered by the user. They are set to example values for security reasons
-	paramNames = ['username','password']
-	paramData = ['username@example.com','Example_Password_1']
+	#The username and password are read in from an external file called auth-codes.txt
+	try:
+		from AuthCodes import *
+		auth = AuthCodes()
+		paramNames = auth.codelist()
+		paramData = auth.authlist()
+	except ImportError:
+		print('Failure to import authorization file')
+		username = input('What is your username?')
+		password = input('What is your password?')
+		paramNames = ['username','password']
+		paramData = [username,password]
 	
 	#This part of the method calls urlEncode method from Encode.py to encode webAddress w/ parameters & return
 	return urlEncode(webAddress,instapaper,paramNames,paramData)
@@ -42,7 +54,10 @@ def getInstapaperURL(webAddress):
 #This method executes the URL that adds desired url to Instapaper through url encoded input URL
 def addToInstapaper(URL):
 	#clear the clipboard, since that's no longer necessary
-	clipboard.set('')
+	try:
+		clipboard.set('')
+	except:
+		print('Cannot clear clipboard')
 	
 	#Library that pythonista can use to open urls
 	import webbrowser
@@ -53,19 +68,22 @@ def addToInstapaper(URL):
 #Main method
 def main():
 	#Check to see if the main method is being run from the share sheet
-	if not appex.is_running_extension():
-		#if this if statement is true, clarify that we're running in the app
-		print('Running in Pythonista app, using test data...\n')
-		
-		#get the clipboard for the url
-		url = str(clipboard.get())
-		
-		#check that the clipboard is a url. if not, set as None for error production
-		if not url.startswith('http'):
-			url = None
-	else:
-		#Get url from app extension if running from extension
-		url = appex.get_url()
+	try:
+		if not appex.is_running_extension():
+			#if this if statement is true, clarify that we're running in the app
+			print('Running in Pythonista app, using test data...\n')
+			
+			#get the clipboard for the url
+			url = str(clipboard.get())
+			
+			#check that the clipboard is a url. if not, set as None for error production
+			if not url.startswith('http'):
+				url = None
+		else:
+			#Get url from app extension if running from extension
+			url = appex.get_url()
+	except:
+		url = input('Enter the URL you want to save')
 	
 	#Check if url has a value
 	if url:
